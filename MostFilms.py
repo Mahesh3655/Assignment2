9669b8c5545a75e342835b20559dbe1101ee1047
Mahesh <saimaheshvasarla@gmail.com>
2022-06-30 08:52:28 +0530

    a
    genre which had the most number of films released per year between the years 2010 and 2018

--- MostFilms.py
+++ MostFilms.py
@@ -1,1 +1,31 @@
+# importing json and urllib library
 import json
+from urllib.request import urlopen
+def main():
+    try:
+        # Storing URL in url
+        url = "https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json"
+        # storing the url response
+        response = urlopen(url)
+        # storing JSON response from url in data
+        data = json.loads(response.read())
+    except:
+        print("Some Error Occured ! ")
+        # creating empty dictonary
+
+    d ={}
+    #iterating through thr each set in the data
+    for i in data:
+        year = i["year"]
+        if year >= 2010:
+            #creating dictonary with key as year if it doesnot exist
+            if year not in d:
+                d[year] = {}
+                #counting the genres in each year
+            for k in i["genres"]:
+                d[year][k] = d[year].get(k,0)+1
+    for y in d:
+        print("In year ",y,"Most of the films released in genre",max(d[y],key=d[y].get))
+    
+if __name__ =="__main__":
+    main()
